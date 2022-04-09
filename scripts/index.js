d3d10007f28c374d0d2ff5e0fdecf8dad04cf684
juliastrukova <julia.strukova.04@gmail.com>
2022-04-10 01:20:45 +0300

    fix js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+var slideInterval = setInterval(nextSlide,10000);
 
 function nextSlide(){
   slides[currentSlide].className = 'slide';
